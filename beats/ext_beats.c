54a064360f961364897bf866b348c41ef2ba8866
z5368211 <z5368211@cse.unsw.edu.au>
2021-04-12 13:33:57 +1000

    start ext

---
+++ beats/ext_beats.c
@@ -0,0 +1,47 @@
+// Assignment 2 21T1 COMP1511: CS bEats
+// beats.c
+//
+// This program was written by YOUR-NAME-HERE (z5555555)
+// on INSERT-DATE-HERE
+//
+// Version 1.0.0: Assignment released.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+// Add any extra #includes your code needs here.
+//
+// But note you are not permitted to use functions from string.h
+// so do not #include <string.h>
+
+#include "ext_beats.h"
+
+/////////////////////////////////////////////////////////////////////
+//      TODO: Replace this with the contents of your beats.c       //
+/////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////
+//                Extension -- Stage 4 Functions                      //
+////////////////////////////////////////////////////////////////////////
+
+// Merge `beats_to_merge` beats into `merged_beats`
+void merge_beats(Track track, int beats_to_merge, int merged_beats) {
+    printf("merge_beats not implemented yet.\n");
+}
+
+////////////////////////////////////////////////////////////////////////
+//                Extension -- Stage 5 Functions                      //
+////////////////////////////////////////////////////////////////////////
+
+void save_track(Track track, char *name) {
+    printf("save_track not implemented yet.\n");
+
+}
+
+Track load_track(char *name) {
+    printf("load_track not implemented yet.\n");
+
+    return NULL;
+}
